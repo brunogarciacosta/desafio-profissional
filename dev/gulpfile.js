4e113a241cad0b2c7318833d0c768123f80738e1
brunogarciacv <brunogarciacosta@gmail.com>
2017-05-17 00:56:53 -0300

    Gulp, Pug & Sass

---
+++ dev/gulpfile.js
@@ -0,0 +1,13 @@
+const gulp = require('gulp');
+const pug = require('gulp-pug');
+const sass = require('gulp-sass');
+
+gulp.task ('pug', function(){
+ return gulp.src('/*.pug').pipe(pug({pretty: true})).pipe( gulp.dest('../'));
+});
+
+gulp.task('sass', function () {
+  return gulp.src('*.sass')
+    .pipe(sass())
+    .pipe(gulp.dest('../assets/'));
+});
