1aec66e29d4141c4adab9327fa0fb53308642844
brunogarciacv <brunogarciacosta@gmail.com>
2017-05-24 18:45:34 -0300

    Page SO

--- dev/gulpfile.js
+++ dev/gulpfile.js
@@ -3,11 +3,16 @@ const pug = require('gulp-pug');
 const sass = require('gulp-sass');
 
 gulp.task ('pug', function(){
- return gulp.src('/*.pug').pipe(pug({pretty: true})).pipe( gulp.dest('../'));
+ return gulp.src('*.pug').pipe(pug({pretty: true})).pipe( gulp.dest('../'));
 });
 
 gulp.task('sass', function () {
-  return gulp.src('*.sass')
-    .pipe(sass())
-    .pipe(gulp.dest('../assets/'));
+	return gulp.src('*.sass')
+		.pipe(sass())
+		.pipe(gulp.dest('../assets/'));
+});
+
+gulp.task('start', function() {
+		gulp.watch('*.pug', ['pug']);
+		gulp.watch('*.sass', ['sass']);
 });
